d50c7af216abf35f176a469bbf5bdca7d5c239b3
aRaimondo <113465657+aRaimondo@users.noreply.github.com>
2023-04-07 23:44:20 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,23 +11,28 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
+  
    button = createButton('Clicca qui!');                                                    //bottone per fullscreen (ATTENZIONE: DA IOS NON FUNZIONA ANCHE DANDO IL CONSENSO)
    button.size(80,20);
-   button.position((windowWidth/2)-40, windowHeight/2);
+   button.position((windowWidth/2)-65, windowHeight/2);
    button.mousePressed(scarica)
 }
 
 function draw() {
   background("yellow");
+  push()
+  imageMode(CORNER);
   if(windowHeight/windowWidth<=sfondo2.height/sfondo2.width){                       //immagine di sfondo adattabile a dimensione
     image(sfondo2, 0, 0, windowWidth, sfondo2.height*windowWidth/sfondo2.width);
   }else{
     image(sfondo2, 0, 0, sfondo2.width*windowHeight/sfondo2.height, windowHeight);
   }
+  pop()
   x = windowWidth/2
   push()
   imageMode(CENTER)
-  image(ditosx, 57+(windowWidth/3), windowHeight/2,ditosx.width*0.2, ditosx.height*0.2)
+  image(ditosx, (windowWidth/3), windowHeight/2,ditosx.width*0.2, ditosx.height*0.2)
   image(ditodx, 2*windowWidth/3, windowHeight/2,ditodx.width*0.2, ditodx.height*0.2)
   image(balloon, windowWidth/7, windowHeight/5, balloon.width*0.5, balloon.height*0.5)
   image(balloon, 6*windowWidth/7, windowHeight/5, balloon.width*0.5, balloon.height*0.5)
